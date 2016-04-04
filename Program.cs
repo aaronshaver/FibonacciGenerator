8582b67d895b888e12c62b1d2047fbb1c0af225d
Aaron Shaver <AaronShaverPDX@gmail.com>
2016-04-04 16:04:31 -0700

    Refactored main Program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FibonacciGenerator
 {
@@ -6,8 +7,42 @@ namespace FibonacciGenerator
     {
         public static void Main(string[] arguments)
         {
-            Console.WriteLine("How many Fibonacci numbers would you like to print? ");
-            var input = Console.ReadLine();
-        } 
+            var iterations = GetNumberOfIterations();
+            var numbers = Fibonacci.GetNumbers(iterations);
+            var numbersText = GetFormattedNumbers(numbers);
+            PrintOutput(iterations, numbersText);
+        }
+
+        private static int GetNumberOfIterations()
+        {
+            int iterations;
+            bool validNumber;
+            do
+            {
+                Console.WriteLine("How many Fibonacci numbers would you like to print? ");
+                var rawInput = Console.ReadLine();
+                validNumber = int.TryParse(rawInput, out iterations);
+            } while (!validNumber);
+            return iterations;
+        }
+
+        private static string GetFormattedNumbers(List<int> numbers)
+        {
+            var numbersText = "";
+            foreach (var number in numbers)
+            {
+                numbersText += number + ", ";
+            }
+            numbersText = numbersText.TrimEnd(',', ' ');
+            return numbersText;
+        }
+
+        private static void PrintOutput(int iterations, string numbersText)
+        {
+            Console.WriteLine($"\nHere are the {iterations} Fibonacci number(s) you asked for:");
+            Console.WriteLine(numbersText);
+            Console.WriteLine("\nPress <Enter> key to continue...");
+            Console.ReadLine();
+        }
     }
 }
