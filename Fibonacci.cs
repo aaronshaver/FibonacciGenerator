978250160637ccdf104af7e6252e66abbfcf5fe9
Aaron Shaver <AaronShaverPDX@gmail.com>
2016-04-04 14:54:45 -0700

    Add main class

---
+++ Fibonacci.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace fibonacci_generator_vs
+{
+    public static class Fibonacci
+    {
+        public static List<int> GetNumbers(int iterations)
+        {
+            var result = new List<int> { 0 };
+            return result;
+
+            var numbers = new List<int>();
+            var previousNumber = 0;
+            var nextNumber = 1;
+            Console.WriteLine(previousNumber);
+            do
+            {
+                numbers.Add(nextNumber);
+                var currentNumber = nextNumber;
+                nextNumber += previousNumber;
+                previousNumber = currentNumber;
+            } while (nextNumber < iterations);
+            return numbers;
+        }
+    }
+}
