3ea3b43f0cbab7a6e7a5a22c044c7cb493e50d0a
Aaron Shaver <AaronShaverPDX@gmail.com>
2016-04-04 14:59:08 -0700

    Namespace changes

--- Fibonacci.cs
+++ Fibonacci.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace fibonacci_generator_vs
+namespace FibonacciGenerator
 {
     public static class Fibonacci
     {
--- FibonacciTests.cs
+++ FibonacciTests.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace fibonacci_generator_vs
+namespace FibonacciGenerator
 {
     [TestClass]
     public class FibonacciTests
